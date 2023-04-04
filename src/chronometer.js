474d6d23841a0ef3274b5133bc4648b991d7f4ce
Gus <august.colonna@gmail.com>
2023-04-04 13:41:26 +0200

    chronometer.JS done + bonus

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,50 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0
+    this.intervalId = null
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+    this.intervalId = setInterval(() => {
+      if(printTimeCallback){
+        printTimeCallback()
+      }
+      this.currentTime += 1
+    }, 1000)
   }
 
   getMinutes() {
-    // ... your code goes here
+    this.minutes = this.currentTime/60
+    return Math.floor(this.minutes)
   }
 
   getSeconds() {
-    // ... your code goes here
+    this.seconds = this.currentTime % 60
+    return Math.floor(this.seconds)
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    let stringVal = value.toString()
+    
+    if(stringVal.length === 1){
+      return "0" + stringVal;
+    }
+    if(stringVal.length === 2){
+      return stringVal;
+    }
   }
 
   stop() {
-    // ... your code goes here
+   return clearInterval(this.intervalId)
   }
-
   reset() {
-    // ... your code goes here
+    this.currentTime = 0
   }
 
   split() {
-    // ... your code goes here
+    let stringMinutes = this.computeTwoDigitNumber(this.getMinutes())
+    let stringSeconds = this.computeTwoDigitNumber(this.getSeconds())
+    let timeFormat = `${stringMinutes}:${stringSeconds}`
+    return timeFormat;
   }
 }
